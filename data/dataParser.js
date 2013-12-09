cd86225b1d22cb78fc3f99237e627e89fec02ca9
Hao-Kuang Tsai <kuang.con@gmail.com>
2013-12-10 01:46:20 +0800

    version v0.8.1
    Add counter.
    Handle more keyboard event and situation

--- data/dataParser.js
+++ data/dataParser.js
@@ -8,13 +8,30 @@ var items = document.getElementsByClassName('item-summary');
 
 var current = -9999;
 
+function checkOpenItem(i) {
+    var opened = document.getElementsByClassName('itembody open');
+
+    if (opened.length == i) {
+        return true;
+    }
+    while (opened.length > 0) {
+        opened[0].getElementsByTagName('a')[0].click();
+    }
+    current = -9999;
+    return false;
+}
+
 function nextItem(i) {
+    if (current<0 || !checkOpenItem(0)) {
+        i = 0;
+    }
+
     console.log("Click on " + i);
     console.log("article caseversion" + articles[i].id);
 
     if (current >= 0 && priority[current].innerHTML.charAt(0) == "*") {
         pr = priority[current].innerHTML.substr(1);
-        
+
         var setting = {};
         setting.id = articles[current].id;
         setting.pr = pr;
@@ -27,45 +44,64 @@ function nextItem(i) {
     items[current].click();
 }
 
-function changePriority(i, pr) {
-    priority[i].innerHTML = "*"+pr;
- }
+function countPriority() {
+    var a = [0, 0, 0, 0, 0];
+    for (var i = 0 ; i < priority.length ; i++) {
+        if (isNaN(priority[i].innerHTML)) {
+            a[0] += 1;
+        }
+        else {
+            a[parseInt(priority[i].innerHTML)] += 1;
+        }
+    }
+    var result = "None: "+a[0]+", P1: "+a[1]+", P2: "+a[2]+", P3: "+a[3]+", P4: "+a[4]+", in "+priority.length;
+    self.port.emit("setContent", result);
+    console.log("MozTizer: " + result);
+}
 
-document.addEventListener("keyup", function (e) {
+function changePriority(i, pr) {
+    if (checkOpenItem(1)) {
+        countPriority();
+        priority[i].innerHTML = "*" + pr;
+    }
+}
+
+self.port.on("item-click", function(i) {
+    nextItem(i);
+});
+
+document.addEventListener("keyup", function(e) {
     if (e.keyCode == 13 || e.keyCode == 39 || e.keyCode == 40) {
         console.log("Show Next");
 
         items[current].click();
-        nextItem(current+1);
-    }
-    else if (e.keyCode == 37 || e.keyCode == 38) {
+        nextItem(current + 1);
+    } else if (e.keyCode == 37 || e.keyCode == 38) {
         console.log("Show Previous");
 
         items[current].click();
-        nextItem(current-1);
-    }
-    else if (e.keyCode == 49) {
+        nextItem(current - 1);
+    } else if (e.keyCode == 49) {
         console.log("Priority 1");
         changePriority(current, "1");
-    }
-    else if (e.keyCode == 50) {
+    } else if (e.keyCode == 50) {
         console.log("Priority 2");
         changePriority(current, "2");
-    }
-    else if (e.keyCode == 51) {
+    } else if (e.keyCode == 51) {
         console.log("Priority 3");
         changePriority(current, "3");
-    }
-    else if (e.keyCode == 52) {
+    } else if (e.keyCode == 52) {
         console.log("Priority 4");
         changePriority(current, "4");
-    }
-    else if (e.keyCode == 78 || e.keyCode == 83) {
+    } else if (e.keyCode == 78 || e.keyCode == 83) {
         console.log("None");
         changePriority(current, "None");
     }
 }, false);
 
-self.port.on("item-click", function(i) {
-    nextItem(i);
-});
+// document.getElementsByClassName('itembody details').addEventListener("click", function(e) {
+
+// });
+
+nextItem(0);
+countPriority();
